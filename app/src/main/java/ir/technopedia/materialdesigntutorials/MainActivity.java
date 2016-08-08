d04d6a06a3742c1c894e0b352f657524d0c52d39
Mehrdad Garebaghi <m.garebaghi@gmail.com>
2016-08-09 01:30:41 +0430

    second lesson : flat buttons added

--- app/src/main/java/ir/technopedia/materialdesigntutorials/MainActivity.java
+++ app/src/main/java/ir/technopedia/materialdesigntutorials/MainActivity.java
@@ -8,23 +8,27 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnButton;
+    Button btnRaisedButton, btnFlatButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnButton = (Button) findViewById(R.id.btn_button);
-        btnButton.setOnClickListener(this);
+        btnRaisedButton = (Button) findViewById(R.id.btn_raised_button);
+        btnRaisedButton.setOnClickListener(this);
+        btnFlatButton = (Button) findViewById(R.id.btn_flat_button);
+        btnFlatButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
 
-        if (id == R.id.btn_button) {
-            startActivity(new Intent(getBaseContext(), ButtonActivity.class));
+        if (id == R.id.btn_raised_button) {
+            startActivity(new Intent(getBaseContext(), RaisedButtonActivity.class));
+        } else if (id == R.id.btn_flat_button) {
+            startActivity(new Intent(getBaseContext(), FlatButtonActivity.class));
         }
     }
 }
